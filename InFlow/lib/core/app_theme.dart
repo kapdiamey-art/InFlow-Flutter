0500f4477fdbf84c99962f1dcfcb0cf1dc4cc0cf
kapdiamey-art <kapdiamey@gmail.com>
2026-04-25 14:09:31 +0530

    Constructor Fixed

--- InFlow/lib/core/app_theme.dart
+++ InFlow/lib/core/app_theme.dart
@@ -36,7 +36,9 @@ class AppTheme {
       cardTheme: const CardThemeData(
         color: AppColors.surface,
         elevation: 0,
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(20)),
+        ),
       ),
       inputDecorationTheme: InputDecorationTheme(
         filled: true,
